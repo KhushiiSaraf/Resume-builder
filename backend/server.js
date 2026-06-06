5e50ecbd3668b95cacbd02f35429b093efbf2d77
Khushi <khushi1saraf@gmail.com>
2026-06-06 19:16:34 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 require('dotenv').config();
-const { generateInterviewReport } = require('./services/ai.service');
-
+const port = process.env.PORT || 3000
 
 connectDB();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
