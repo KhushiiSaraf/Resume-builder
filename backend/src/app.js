b54c4c73495be35b3587f63677524c87c3d8f83d
Khushi <khushi1saraf@gmail.com>
2026-06-08 21:49:31 +0530

    api/health route added for cronjob

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,5 +19,11 @@ const interviewRouter = require('./routes/interview.routes')
 // use all routes
 app.use('/api/auth', authRouter);
 app.use('/api/interview', interviewRouter);
+app.get('/api/health', (req, res) => {
+    res.status(200).json({
+        status: 'ok',
+        message: 'Server is running'
+    });
+});
 
 module.exports = app;
